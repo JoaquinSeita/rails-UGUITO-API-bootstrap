5069317ad633baa80223895acf308e36b0edbd83
Joaquin Garcia del Rio <jogarcia@itba.edu.ar>
2024-07-26 17:43:35 -0300

    feat: note model and migration
    Created Note table through a migration and its model

---
+++ app/models/note.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string           not null
+#  content    :text             not null
+#  note_type  :string           not null
+#  user_id    :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Note < ApplicationRecord
+  has_one :utility, through: :user
+  belongs_to :user
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :note_type, presence: true
+end
