2491a18421474b16a98cabadc2d8ccb03ccdf4ec
Joaquin Garcia del Rio <jogarcia@itba.edu.ar>
2024-07-29 17:40:50 -0300

    feat: note content_length validation

--- app/models/note.rb
+++ app/models/note.rb
@@ -14,5 +14,54 @@ class Note < ApplicationRecord
   belongs_to :user
   has_one :utility, through: :user
   enum note_type: { review: 0, critique: 1 }
+  validate :content_length
   validates :user_id, :title, :content, presence: true
+
+  private
+
+  def word_count
+    content.split(' ').size
+  end
+
+  def validate_max_words(max_word_counts)
+    return unless note_type == 'review' && word_count > max_word_counts['review']
+    errors.add(:content, I18n.t('activerecord.models.note.validations.content_length'))
+  end
+
+  def utility_validation(max_word_counts, thresholds)
+    word_count = self.word_count
+    validate_max_words(max_word_counts)
+
+    case word_count
+    when 0..thresholds['short']
+      :short
+    when (thresholds['short'] + 1)..thresholds['medium']
+      :medium
+    else
+      :long
+    end
+  end
+
+  def validate_north_utility
+    utility_validation(
+      { 'review' => 50 },
+      { 'short' => 50, 'medium' => 100 }
+    )
+  end
+
+  def validate_south_utility
+    utility_validation(
+      { 'review' => 60 },
+      { 'short' => 60, 'medium' => 120 }
+    )
+  end
+
+  def content_length
+    case utility.code
+    when 1
+      validate_north_utility
+    when 2
+      validate_south_utility
+    end
+  end
 end
