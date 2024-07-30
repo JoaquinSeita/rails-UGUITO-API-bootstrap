5906c098326035f95fb9da593cb55a812042ac09
Joaquin Garcia del Rio <jogarcia@itba.edu.ar>
2024-07-30 12:31:09 -0300

    feat: initial node_spec

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -13,6 +13,34 @@ RSpec.describe Note, type: :model do
     it { is_expected.to validate_presence_of(value) }
   end
 
+  describe '#validate_content_word_count' do
+    it 'validates word count' do
+      expect(subject.word_count).to be_kind_of(Integer)
+    end
+
+    it 'validates content length' do
+      expect(subject.content_length).to be_in(%w[short medium long])
+    end
+
+    context 'when note type is review and content length is not short' do
+      let(:note) { build(:note, note_type: :review, content: '') }
+
+      it 'adds an error to the content attribute' do
+        note.valid?
+        expect(note.errors[:content]).to include(I18n.t('content_length_error'))
+      end
+    end
+
+    context 'when note type is review and content length is short' do
+      let(:note) { build(:note, note_type: :review, content: 'This is a short content') }
+
+      it 'does not add an error to the content attribute' do
+        note.valid?
+        expect(note.errors[:content]).not_to include(I18n.t('content_length_error'))
+      end
+    end
+  end
+
   it 'has a valid factory' do
     expect(subject).to be_valid
   end
