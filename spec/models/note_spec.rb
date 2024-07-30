11f23d800b459d12b0a36e89f6ad2417971240f1
Joaquín García del Río <45571740+JoacoGDR@users.noreply.github.com>
2024-07-30 08:41:03 -0300

    feat: [TR-458] note model
    * feat: note model and migration
    
    Created Note table through a migration and its model
    
    * feat: user has_many notes
    
    Added the one to many relation to user model. Also fixed the order of fields in the note model.
    
    * fix: reverted changes to Gemfile.lock
    
    * fix: consistent validations and factory tests
    
    Defined Factory Bot model and added test to note_spec. Updated seed file to populate a few notes. Fixed data type of content in migration and relation consistencies with model.
    
    * fix: note_type enum
    
    Changed note_type to enum. Validated user_id presence too.
    
    * fix: outdated note_spec fixed
    
    * fix: note_type presence

---
+++ spec/models/note_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+  subject(:note) do
+    create(:note)
+  end
+
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_one(:utility).through(:user) }
+  it { is_expected.to define_enum_for(:note_type).with_values(%i[review critique]) }
+
+  %i[user_id title content note_type].each do |value|
+    it { is_expected.to validate_presence_of(value) }
+  end
+
+  it 'has a valid factory' do
+    expect(subject).to be_valid
+  end
+end
