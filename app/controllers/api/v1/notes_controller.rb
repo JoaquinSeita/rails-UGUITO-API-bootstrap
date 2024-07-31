80d17bdc607e6d001b36a865080e310bcd3ca289
Joaquin Garcia del Rio <jogarcia@itba.edu.ar>
2024-07-31 11:06:30 -0300

    feat: note controller authentication

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,27 +1,35 @@
 module Api
   module V1
     class NotesController < ApplicationController
+      before_action :authenticate_user!
+
       def index
         render json: notes_filtered, status: :ok, each_serializer: IndexNoteSerializer
-      end
-
-      def notes_filtered
-        Note.where(filtering_params)
-            .order(created_at: params[:order] || :desc)
-            .page(params[:page])
-            .per(params[:page_size])
-      end
-
-      def filtering_params
-        params.permit(%i[note_type])
       end
 
       def show
         render json: show_note, status: :ok, serializer: NoteSerializer
       end
 
+      private
+
+      def filtering_params
+        params.permit(%i[note_type])
+      end
+
+      def notes_filtered
+        notes.where(filtering_params)
+             .order(created_at: params[:order] || :desc)
+             .page(params[:page])
+             .per(params[:page_size])
+      end
+
       def show_note
-        Note.find(params.require(:id))
+        notes.find(params.require(:id))
+      end
+
+      def notes
+        current_user.notes
       end
     end
   end
