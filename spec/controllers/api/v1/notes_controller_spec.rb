1bd16a5b40f1fc663d549f807be39558cc592c14
Joaquin Garcia del Rio <jogarcia@itba.edu.ar>
2024-08-01 17:42:34 -0300

    fix: note controller spec updated

--- spec/controllers/api/v1/notes_controller_spec.rb
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -1,37 +1,44 @@
 require 'rails_helper'
+shared_examples 'returns expected result' do
+  it 'responds with the expected notes' do
+    expect(response_body.to_json).to eq(expected)
+  end
+
+  it 'responds with 200 status' do
+    expect(response).to have_http_status(:ok)
+  end
+end
+
+shared_examples 'returns 404' do
+  it 'responds with 404 status' do
+    expect(response).to have_http_status(:not_found)
+  end
+end
 
 describe Api::V1::NotesController, type: :controller do
+  let(:other_user) { create(:user) }
+  let(:other_user_note_count) { Faker::Number.between(from: 1, to: 100) }
+  let(:other_user_notes) { create_list(:note, other_user_note_count, user: other_user) }
+  let(:user_note_count) { Faker::Number.between(from: 1, to: 100) }
+  let(:user_notes) { create_list(:note, user_note_count, user: user) }
+
   describe 'GET #index' do
-    let(:user_notes) { create_list(:note, 5, user: user) }
+    let(:page)       { 1 }
+    let(:page_size)  { Faker::Number.between(from: 1, to: 100) }
+    let(:required_params)    { { note_type: %i[review critique].sample, page: page, page_size: page_size, order: %i[desc asc].sample } }
 
     context 'when there is a user logged in' do
       include_context 'with authenticated user'
 
-      let!(:expected) do
+      let(:expected) do
         ActiveModel::Serializer::CollectionSerializer.new(notes_expected,
-                                                          serializer: IndexNoteSerializer).to_json
+                                                          serializer: BriefNoteSerializer).to_json
       end
 
-      context 'when fetching all the notes for user' do
-        let(:notes_expected) { user_notes.reverse }
+      context 'when fetching notes with all required params' do
+        before { get :index, params: required_params }
 
-        before { get :index }
-
-        it 'responds with the expected notes json' do
-          expect(response_body.to_json).to eq(expected)
-        end
-
-        it 'responds with 200 status' do
-          expect(response).to have_http_status(:ok)
-        end
-      end
-
-      context 'when fetching notes with page and page size params' do
-        let(:page)            { 1 }
-        let(:page_size)       { 2 }
-        let(:notes_expected) { user_notes.reverse.first(2) }
-
-        before { get :index, params: { page: page, page_size: page_size } }
+        let(:notes_expected) { Note.where(note_type: required_params[:note_type]).order(created_at: required_params[:order]).with_pagination(required_params[:page], required_params[:page_size]) }
 
         it 'responds with the expected notes' do
           expect(response_body.to_json).to eq(expected)
@@ -42,34 +49,23 @@ describe Api::V1::NotesController, type: :controller do
         end
       end
 
-      context 'when fetching notes using filters' do
-        let(:note_type) { %w[review critique].sample }
+      context 'when fetching notes with a missing required param' do
+        let(:missing_param)  { required_params.keys.sample }
 
-        let!(:typed_notes) { create_list(:note, 2, user: user, note_type: note_type) }
-        let(:notes_expected) { typed_notes.reverse }
+        before { get :index, params: required_params.except(missing_param) }
 
-        before { get :index, params: { note_type: note_type } }
-
-        it 'responds with expected notes' do
-          expect(response_body.to_json).to eq(expected)
-        end
-
-        it 'responds with 200 status' do
-          expect(response).to have_http_status(:ok)
+        it 'returns status code bad request' do
+          expect(response).to have_http_status(:bad_request)
         end
       end
 
-      context 'when fetching notes using order param' do
-        let(:notes_expected) { user_notes }
+      context 'when fetching notes with an invalid note type' do
+        let(:invalid_note_type) { :invalid_type }
 
-        before { get :index, params: { order: :asc } }
+        before { get :index, params: required_params.merge(note_type: invalid_note_type) }
 
-        it 'responds with expected notes' do
-          expect(response_body.to_json).to eq(expected)
-        end
-
-        it 'responds with 200 status' do
-          expect(response).to have_http_status(:ok)
+        it 'returns status code unprocessable entity' do
+          expect(response).to have_http_status(:unprocessable_entity)
         end
       end
     end
@@ -84,25 +80,16 @@ describe Api::V1::NotesController, type: :controller do
   end
 
   describe 'GET #show' do
-    let(:user_notes) { create_list(:note, 5, user: user) }
-
     context 'when there is a user logged in' do
       include_context 'with authenticated user'
 
-      let(:note) { user_notes.sample }
-
       context 'when fetching a note' do
-        let(:note_expected) { NoteSerializer.new(note).to_json }
+        let(:note) { user_notes.sample }
+        let(:expected) { NoteSerializer.new(note).to_json }
 
         before { get :show, params: { id: note.id } }
 
-        it 'responds with the expected note json' do
-          expect(response_body.to_json).to eq(note_expected)
-        end
-
-        it 'responds with 200 status' do
-          expect(response).to have_http_status(:ok)
-        end
+        it_behaves_like 'returns expected result'
       end
 
       context 'when fetching an invalid note' do
@@ -110,9 +97,15 @@ describe Api::V1::NotesController, type: :controller do
 
         before { get :show, params: { id: note.id } }
 
-        it 'responds with 404 status' do
-          expect(response).to have_http_status(:not_found)
-        end
+        it_behaves_like 'returns 404'
+      end
+
+      context 'when fetching another user\'s note' do
+        let(:other_user_note) { other_user_notes.sample }
+
+        before { get :show, params: { id: other_user_note.id } }
+
+        it_behaves_like 'returns 404'
       end
     end
 
